1b00f656b60ad205b20b1e2d178a9203eabab01b
David Rose <david.rose@perkinelmer.com>
2019-07-25 16:58:05 -0400

    Added AutomationChecker.

---
+++ ParseXaml2/AutomationIdChecker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ParseXaml2
+{
+    public class AutomationIdChecker
+    {
+        private string _startingPath;
+        private Action<string> _output;
+
+        public AutomationIdChecker(string startingPath, Action<string> output)
+        {
+            _startingPath = startingPath;
+            _output = output;
+        }
+
+        public void ParseXaml(string path)
+        {
+            using (var fs = File.Open(path, FileMode.Open, FileAccess.Read))
+            {
+                var settings = new XmlReaderSettings();
+                using (var reader = XmlReader.Create(fs, settings))
+                {
+                    while (reader.Read())
+                    {
+                        switch (reader.NodeType)
+                        {
+                            case XmlNodeType.Element:
+                                var element = $"Start Element {reader.Name}";
+                                if (reader.HasAttributes)
+                                {
+                                    if (reader.MoveToAttribute("Name"))
+                                    {
+                                        var attribute = $"Attribute name: {reader.Value}";
+                                        if (!reader.MoveToAttribute("AutomationProperties.AutomationId"))
+                                        {
+                                            _output(element);
+                                            _output(attribute);
+                                            _output("No Automation Id");
+                                        }
+
+                                        _output("");
+                                    }
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- ParseXaml2/Program.cs
+++ ParseXaml2/Program.cs
@@ -18,42 +18,8 @@ namespace ParseXaml2
         {
             var path = @"..\SelectSubsystemHardware.xaml";
 
-            ParseXaml(path);
-        }
-
-        private static void ParseXaml(string path)
-        {
-            using (var fs = File.Open(path, FileMode.Open, FileAccess.Read))
-            {
-                var settings = new XmlReaderSettings();
-                using (var reader = XmlReader.Create(fs, settings))
-                {
-                    while (reader.Read())
-                    {
-                        switch (reader.NodeType)
-                        {
-                            case XmlNodeType.Element:
-                                var element = $"Start Element {reader.Name}";
-                                if (reader.HasAttributes)
-                                {
-                                    if (reader.MoveToAttribute("Name"))
-                                    {
-                                        var attribute = $"Attribute name: {reader.Value}";
-                                        if (!reader.MoveToAttribute("AutomationProperties.AutomationId"))
-                                        {
-                                            Console.WriteLine(element);
-                                            Console.WriteLine(attribute);
-                                            Console.WriteLine("No Automation Id");
-                                        }
-
-                                        Console.WriteLine();
-                                    }
-                                }
-                                break;
-                        }
-                    }
-                }
-            }
+            var checker = new AutomationIdChecker(path, Console.WriteLine);
+            checker.ParseXaml(path);
         }
     }
 }
